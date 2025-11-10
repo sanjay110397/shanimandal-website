0717e9ab4e67c7914c85a74d1b25a653c01e7753
sanjay rajput <sanjay110397@gmail.com>
2025-11-11 01:02:24 +0530

    Fix nested Router issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { Navbar } from './components/Navbar';
 import { Footer } from './components/Footer';
 import { ScrollToTop } from './components/ScrollToTop';
@@ -10,20 +10,18 @@ import './i18n';
 
 export default function App() {
   return (
-    <Router>
-      <div className="flex flex-col min-h-screen">
-        <Navbar />
-        <main className="flex-1">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/history" element={<History />} />
-            <Route path="/gallery" element={<Gallery />} />
-            <Route path="/visit" element={<Visit />} />
-          </Routes>
-        </main>
-        <Footer />
-        <ScrollToTop />
-      </div>
-    </Router>
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <main className="flex-1">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/history" element={<History />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/visit" element={<Visit />} />
+        </Routes>
+      </main>
+      <Footer />
+      <ScrollToTop />
+    </div>
   );
 }
